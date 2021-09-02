50124d84e2bb90aefac72d9e81f4075f0a4aa008
mjones12209 <m.jones12209@gmail.com>
2021-09-02 10:54:03 -0400

    added some styling for contactform

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import AppRouter from './routes/AppRouter';
 function App() {
   return (
     <>
-   <AppRouter />
-
-   
-  
+      <AppRouter />  
     </>
   )
 }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import './ContactForm.css';
 
 const ContactForm = () => {
   return (
@@ -8,23 +9,19 @@ const ContactForm = () => {
         <span className="styles2">Author of Our Son's</span>{" "}
         <span className="styledtext">Dejohn Huffman</span>
       </h1>
-      <div className="d-flex justify-content-center redLinear">
-        <Form className="col-md-6 shadow p-3 mb-5 bg-white borderRadius">
+      <div className="d-flex justify-content-center">
+        <Form className="redLinear p-3 col-md-6 bg-white borderRadius redLinear">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Email address</Form.Label>
             <Form.Control
               type="email"
               placeholder="name@example.com"
-              className="shadow p-3 mb-5 redLinear"
+              className="shadow p-3 mb-5"
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Example textarea</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              className="shadow p-3 mb-5 redLinear"
-            />
+            <Form.Control as="textarea" rows={3} className="shadow p-3 mb-5" />
           </Form.Group>
         </Form>
       </div>
