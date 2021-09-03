8177d82a24dc7d1e2e618e9fd44d821332ec4675
Dejohn Huffman <dejohnhuffman1@gmail.com>
2021-09-03 10:02:22 -0400

    hfh

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import "./ContactForm.css";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qs9q9rd",
+        "template_n5yj3la",
+        e.target,
+        "user_fNVY4cfR1XC9tvth8vkxY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
   return (
     <>
       <h1 className="text-center heading1">
@@ -10,7 +30,10 @@ const ContactForm = () => {
         <span className="styledtext">Dejohn Huffman</span>
       </h1>
       <div className="d-flex justify-content-center">
-        <Form className="redLinear p-3 col-md-6 bg-white borderRadius redLinear">
+        <Form
+          onSubmit={sendEmail}
+          className="p-3 col-md-6 bg-white borderRadius redLinear"
+        >
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Email address</Form.Label>
             <Form.Control
@@ -20,7 +43,7 @@ const ContactForm = () => {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
+            <Form.Label>Enter Your Critique or Chapter Summary</Form.Label>
             <Form.Control as="textarea" rows={3} className="shadow p-3 mb-5" />
           </Form.Group>
         </Form>
