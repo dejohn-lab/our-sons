2f8cc699eaf57a7fc9876f3ba3f8f095982976db
Michael Jones <61482297+mjones12209@users.noreply.github.com>
2021-09-09 10:09:41 -0400

    Added User_Name and User_Email name props.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,7 +5,6 @@ import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const sendEmail = (e) => {
-
     e.preventDefault();
 
     emailjs
@@ -23,8 +22,7 @@ const ContactForm = () => {
           console.log(error.text);
         }
       );
-      
-  }
+  };
   return (
     <>
       <h1 className="text-center heading1">
@@ -37,9 +35,12 @@ const ContactForm = () => {
           className="p-3 col-md-6 bg-white borderRadius redLinear"
         >
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" name="user_name" className="mb-2" />
             <Form.Label>Email address</Form.Label>
             <Form.Control
               type="email"
+              name="user_email"
               placeholder="name@example.com"
               className="shadow p-3 mb-5"
             />
@@ -50,6 +51,16 @@ const ContactForm = () => {
           </Form.Group>
           <Button type="submit">Submit</Button>
         </Form>
+        {/* <form className="contact-form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form> */}
       </div>
     </>
   );
